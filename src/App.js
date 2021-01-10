2c61f63e03c63c4e7a7877d916a93f79e0e1340a
Bhat <ash@robhat.com>
2021-01-10 12:50:40 -0800

    testing mobile

--- src/App.js
+++ src/App.js
@@ -3,22 +3,78 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<body>
+  <div className="all">
+  <div className="banner">
+    <div className="banner_content">
+    <img className="calfoundersimage" src="https://ashfromberkeleytown.files.wordpress.com/2021/01/calfounders-1.png"/>
+    <div className="description">
+      <p>this <b>Thursday, January 14th</b>, hosted by Clubhouse</p>
     </div>
+    
+
+    </div>
+  </div>
+
+  <div className="content">
+    <div className="contentHolder">
+    <div className="Context">
+    <h2 className="ContextTitle">Context</h2>
+    <p>This is the first annual town hall of Cal Founders '10 - '20 (past, present, and future) to talk about the state of the world from the entrepreneurial lense.</p>
+    <p>This Town Hall will be open for the public to listen in and by hosted by Clubhouse.</p>
+    </div>
+
+    <div className="agenda">
+      <h2>Agenda</h2>
+      <div className="grid-container">
+        <div className="grid-item firstItem">
+        <ul>
+          <li>6:00p</li>
+          <li>6:10p</li>
+          <li>6:30p</li>
+          <li>6:45p</li>
+          <li>7:00p+</li>
+        </ul>
+        </div>
+        <div className="grid-item">
+        <ul>
+          <li>Opening</li>
+          <li>Introductions</li>
+          <li>Reflecting on 2020</li>
+          <li>Predicting 2021</li>
+          <li>Hang + Chill</li>
+        </ul>         
+        </div> 
+      </div>
+    </div>
+
+    <div className="engager">
+      <div className="buttonDiv firstButton" onclick="location.href='https://calendar.google.com/event?action=TEMPLATE&tmeid=M211YjQ5aHFlcGs3NDN1aHR0YWNhcjRqYWIgYmhhdEBiZXJrZWxleS5lZHU&tmsrc=bhat%40berkeley.edu';">
+        <img className="icon" src="https://ashfromberkeleytown.files.wordpress.com/2021/01/calendar_blue.png"/>
+        <div className="buttonText">
+        Add to
+        <br/>
+        <b>Calendar</b>
+        </div>
+
+      </div>
+      <div className="buttonDiv" onclick="location.href='https://www.joinclubhouse.com/event/gxlOD6qP';">
+        <img className="icon" src="https://www.joinclubhouse.com/static/img/icon_wave.2872aeea710c.png"/>
+
+        <div className="buttonText">
+        Join us on
+        <br/>
+        <b>Clubhouse</b>
+        </div>
+      </div>
+    </div>
+
+    <center><div className="credit">organized by <a className="reference" href="https://twitter.com/Ke5havRao">Keshav</a>, <a className="reference" href="https://twitter.com/aakashadesara">Aakash</a>, and <a className="reference" href="https://twitter.com/theashbhat">Ash</a></div></center>
+
+  </div>
+  </div>
+  </div>
+</body>
   );
 }
 
